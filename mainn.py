874d46bae51c63ceca208806eda9589534135220
timofey11110 <timofey11110@gmail.com>
2026-06-10 16:49:27 +0300

    feat: add footer stration

---
+++ mainn.py
@@ -0,0 +1,36 @@
+# a = 0
+# b = 0
+# while True:
+#     n = int(input())
+#     if n == 0:
+#         break
+#     if n % 4 == 0:
+#         a += n
+#         b += 1
+# if a == 0:
+#     print("NO")
+# else:
+#     print(a/b)
+
+# a = 0
+# b = 0
+# while True:
+#     n = int(input())
+#     if n == 0:
+#         break
+#     if 9 < n < 100:
+#         a += n
+#         b += 1
+# if a == 0:
+#     print("NO")
+# else:
+#     print(a/b)
+
+# a = 0
+# n = int(input())
+# for i in range(n):
+#     num = int(input())
+#     if num % 3 == 0 and num % 10 == 2:
+#         a += num
+# print(a)
+
